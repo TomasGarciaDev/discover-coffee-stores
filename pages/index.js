c33d81a16f95339162961c1de26e6c2d140e5223
Tomas Garcia <tomas.garcia@zolar.de>
2022-12-02 10:06:56 +0100

    CSS for .card  and . cardLayout added

--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,26 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} alt=''/>
         </div>
-        <Card 
-          name='DarkHorse Coffee' 
-          imgUrl='/static/hero-image.png'  
-          href='/coffee-store/darkhorse-coffee'
-        />
+        <div className={styles.cardLayout}>
+          <Card 
+            name='DarkHorse Coffee' 
+            imgUrl='/static/hero-image.png'  
+            href='/coffee-store/darkhorse-coffee'
+            className={styles.card}
+          />
+          <Card 
+            name='DarkHorse Coffee' 
+            imgUrl='/static/hero-image.png'  
+            href='/coffee-store/darkhorse-coffee'
+            className={styles.card}
+          />
+          <Card 
+            name='DarkHorse Coffee' 
+            imgUrl='/static/hero-image.png'  
+            href='/coffee-store/darkhorse-coffee'
+            className={styles.card}
+          /> 
+        </div>
       </main>
     </div>
   )
