dd9b947e8465b823400bb2bb15974dd2ed1c8f8a
Tomas Garcia <tomas.garcia@zolar.de>
2023-01-02 18:06:40 +0100

    Fallback true on getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -18,12 +18,17 @@ export function getStaticProps(staticProps) {
 export function getStaticPaths() {
   return {
     paths: [{ params: { id: "0"}}, { params: { id: "1"}}],
-    fallback: false,
+    fallback: true,
   }
 }
 
 const CoffeeStore = (props) => {
   const router = useRouter()
+  console.log(props)
+  
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
   
   return (
     <div>
