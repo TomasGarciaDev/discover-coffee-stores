eb6848b4b9725af5198eedd3c0040022d0874196
Tomas Garcia <tomas.garcia@zolar.de>
2023-01-28 22:26:33 +0100

    WIP: fetch from Foursquare

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,28 @@ import coffeeStoresData from '../data/coffee-stores.json'
 
 export async function getStaticProps(context) {
   console.log("hi getStaticProps")
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'fsq3iZaspGD6tnr++djD9LXNFyZtFwMChDu2KkohBnMX4NI='
+    }
+  };
+  
+  const response = await fetch('https://api.foursquare.com/v3/places/search?query=coffee&ll=39.53296611001593%2C-0.41184749150409933&limit=6', options);
+  const data = await response.json();
+  
+    // .catch(err => console.error(err));
+
   return {
     props: {
-      coffeeStores: coffeeStoresData,
-    },
-  };
+      coffeeStores: data.results,
+    }
+  }
 }
  
 export default function Home(props) {
-  
   const handleOnBannerBtnClick = () => {
     console.log('hi banner button')
   }
@@ -42,10 +55,10 @@ export default function Home(props) {
               {props.coffeeStores.map(coffeeStore => {
                 return (
                   <Card 
-                    key={coffeeStore.id}
+                    key={coffeeStore.fsq_id}
                     name={coffeeStore.name}
-                    imgUrl={coffeeStore.imgUrl}
-                    href={`/coffee-store/${coffeeStore.id}`}
+                    imgUrl={coffeeStore.imgUrl || "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"}
+                    href={`/coffee-store/${coffeeStore.fsq_id}`}
                     className={styles.card}
                   />
                 );
